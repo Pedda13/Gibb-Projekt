eb86012de7e597337fa087514fc54cbb1cc92f84
Pedda13 <113292672+Pedda13@users.noreply.github.com>
2023-05-05 09:50:41 +0200

    Final Version
    34

--- skript.sh
+++ skript.sh
@@ -1,13 +1,12 @@
 while true 
 do
 UserIPAddress=$(zenity --entry --text="Please enter IP-Adress to Remote control")
-UserUsername=$(zenity --entry --text="Please enter the Username which has root permissions on the remote Computer")
 UserChoice=$(zenity --list --title="Choose what you want to do" --column "Choose" Restart Shutdown)
 
 echo $UserChoice
 if [ $UserChoice == "Restart" ]
 then
-	ssh -t $UserUsername@$UserIPAddress "shutdown now -r"
+	ssh -t root@$UserIPAddress "shutdown now -r"
 	UserAgain=$(zenity --entry --text="Do you want to use another IP?(y/n)")
 	if [ $UserAgain == "y" ]
 		then
@@ -16,7 +15,7 @@ then
 			break
 	fi
 else
-	ssh -t $UserUsername@$UserIPAddress "shutdown now"
+	ssh -t root@$UserIPAddress "shutdown now"
 	UserAgain=$(zenity --entry --text="Do you want to use another IP?(y/n)")
 	if [ $UserAgain == "y" ]
 	then
