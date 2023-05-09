febf3df4bbde4563187674560321aa33be0863d2
Pedda13 <113292672+Pedda13@users.noreply.github.com>
2023-05-09 09:52:13 +0200

    Update skript.sh

--- skript.sh
+++ skript.sh
@@ -1,21 +1,38 @@
 while true 
 do
 UserIPAddress=$(zenity --entry --text="Please enter IP-Adress to Remote control")
-UserChoice=$(zenity --list --title="Choose what you want to do" --column "Choose" Restart Shutdown)
+UserName=$(zenity --entry --text="Please enter an Username which has enough permissions")
+UserChoice=$(zenity --list --title="Choose what you want to do" --column "Choose" Restart Shutdown Custom)
+
+
 
 echo $UserChoice
 if [ $UserChoice == "Restart" ]
 then
-	ssh -t root@$UserIPAddress "shutdown now -r"
+	ssh -t $UserName@$UserIPAddress "sudo shutdown now -r"
 	UserAgain=$(zenity --entry --text="Do you want to use another IP?(y/n)")
 	if [ $UserAgain == "y" ]
-		then
-			continue
-		else
-			break
+	then
+		continue
+	else
+		break
 	fi
-else
-	ssh -t root@$UserIPAddress "shutdown now"
+fi
+if [ $UserChoice == "Shutdown" ]
+then
+	ssh -t $UserName@$UserIPAddress "sudo shutdown now"
+	UserAgain=$(zenity --entry --text="Do you want to use another IP?(y/n)")
+	if [ $UserAgain == "y" ]
+	then
+		continue
+	else
+		break
+	fi
+fi
+if [ $UserChoice == "Custom" ]
+then
+	Usercommand=$(zenity --entry --text="Please enter the command you want to use")
+	ssh -t $UserName@$UserIPAddress "$Usercommand"
 	UserAgain=$(zenity --entry --text="Do you want to use another IP?(y/n)")
 	if [ $UserAgain == "y" ]
 	then
